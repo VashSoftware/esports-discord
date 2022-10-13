e6b9c3e863782b292533745ee25866a818909c85
Stan <stanrunge03@gmail.com>
2022-10-13 17:29:45 +0200

    log messages to database

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,6 +7,13 @@ module.exports = {
 
         // Log message to console
         console.log(`${message.author.username} (${message.guild.name}, #${message.channel.name}): ${message.content}`);
+
+        // Log message to database
+        const {connection} = require('../index.js');
+        const sql = `INSERT INTO message (id, author_discord_id, guild_id, channel_id, content) VALUES ('${message.id}', '${message.author.id}', '${message.guild.id}', '${message.channel.id}', '${message.content}')`;
+        connection.query(sql, function (error, results, fields) {
+            if (error) throw error;
+        });
         
         // Counting channel automod
         if(message.channel.id === process.env.DISCORD_COUNT_CHANNEL_ID) {            
