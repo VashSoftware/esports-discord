a7a97c40aa763f71bd4bb73d153278fd0ff6b5e6
stanrunge <stanrunge03@gmail.com>
2022-10-27 14:13:10 +0200

    Add functionality to emotes command

--- src/commands/emotes.js
+++ src/commands/emotes.js
@@ -5,5 +5,7 @@ module.exports = {
         .setName('emotes')
         .setDescription('Shows all the emotes in the server.'),
     async execute (interaction) {
+        const emotes = interaction.guild.emojis.cache.map(e => e.toString()).join(' ');
+        interaction.reply(emotes);
     }
 }
