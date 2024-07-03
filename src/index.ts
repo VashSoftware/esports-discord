f78f7cadf9e859149e9fa143f2966fc4da9f8037
Stan Runge <stan@stanrunge.dev>
2024-07-03 20:17:35 +0200

    Remove bancho client

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 // Imports
 import { Client, GatewayIntentBits, Collection } from "discord.js";
 import { deployCommands } from "./utils/deployCommands";
-import Banchojs from "bancho.js";
 import { loadEvents } from "./events";
-
-const banchoConnection = new Banchojs.BanchoClient({
-  username: process.env.OSU_IRC_USERNAME!,
-  password: process.env.OSU_IRC_KEY!,
-});
-
-await banchoConnection.connect();
-console.log("Connected to Bancho!");
 
 const discordClient = new Client({
   intents: [
